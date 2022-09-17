d15d3d3b4d18905fc609df5103e32d1bd9b33b54
Meera <banumeerav@gmail.com>
2022-09-17 22:03:17 +0530

    Neuro diversity changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'neuro-diversity',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
